62ecc1a7396bbcd2fcedbe5393095f562ae11cc3
Johannes Berg <johannes@sipsolutions.net>
2020-01-08 21:22:48 +0100

    storage: add storage abstraction
    This will be used by the encrypted repo - we may want to store
    that in arbitrary storage, e.g. cloud storage, and we don't
    really need a lot of API to access it.
    
    Supports the following:
     * writing a file - you can stream it out, there's no way to
       seek back to the beginning and modify a file right now
     * reading a file - this needs to be able to seek in order
       to read arbitrary pieces of the data
     * listing the files - need to generate a list of idx files
       to synchronize a repo back if the cache was removed
     * ability to atomically overwrite a file with a previously
       read one (failing on races)
    
    We may want to extend this in the future to let even real git
    repositories to go through the abstraction, but for now this
    isn't supported and this will be used only by the encrypted
    repo where you can't reasonably mmap() a file anyway since it
    has to be decrypted.
    
    Also add a file-based backend here, with some validation on
    the APIs.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

---
+++ lib/bup/t/tstorage.py
@@ -0,0 +1,117 @@
+
+from __future__ import absolute_import, print_function
+import os
+import struct
+from contextlib import contextmanager
+
+import hashlib
+
+from wvtest import *
+
+from buptest import no_lingering_errors, test_tempdir
+from bup.storage import Kind, FileAlreadyExists, FileNotFound, get_storage
+from bup import git
+
+
+try:
+    # Allow testing this against any kind of storage backend
+    # (for development) - note that you may have to clean up
+    # data inside it after each run manually.
+    repo_conf = os.environ['STORAGE_TEST_CONF']
+except KeyError:
+    repo_conf = None
+
+@contextmanager
+def create_test_config():
+    with test_tempdir(b'enc') as tmpdir:
+        if repo_conf is None:
+            cfgfile = os.path.join(tmpdir, b'repo.conf')
+            cfg = open(cfgfile, 'wb')
+            cfg.write(b'[bup]\n')
+            cfg.write(b'  storage = File\n')
+            cfg.write(b'  path = %s\n' % os.path.join(tmpdir, b'repo'))
+            cfg.write(b'  cachedir = %s\n' % os.path.join(tmpdir, b'cache'))
+            cfg.close()
+            create = True
+        else:
+            wvstart("storage config from %s" % repo_conf)
+            cfgfile = repo_conf
+            create = False
+        class FakeRepo:
+            def config(self, k, opttype=None, default=None):
+                assert isinstance(k, bytes)
+                return git.git_config_get(k, cfg_file=cfgfile,
+                                          opttype=opttype,
+                                          default=default)
+        store = get_storage(FakeRepo(), create=create)
+        yield tmpdir, store
+        del store
+
+@wvtest
+def test_storage_config():
+    with no_lingering_errors(), create_test_config() as (tmpdir, store):
+        wvstart("create a new file")
+        wr = store.get_writer(b'test-storage-overwrite', Kind.CONFIG)
+        wr.write(b'a' * 100)
+        wr.close()
+
+        wvstart("cannot overwrite it")
+        wvexcept(FileAlreadyExists, store.get_writer,
+                 b'test-storage-overwrite', Kind.CONFIG)
+
+        wvstart("replace it atomically")
+        rd = store.get_reader(b'test-storage-overwrite', Kind.CONFIG)
+        wr = store.get_writer(b'test-storage-overwrite', Kind.CONFIG,
+                              overwrite=rd)
+        wr.write(b'b' * 100)
+        wr.close()
+        wr = store.get_writer(b'test-storage-overwrite', Kind.CONFIG,
+                              overwrite=rd)
+        wr.write(b'c' * 100)
+        wr.abort()
+        wvpasseq(rd.read(), b'a' * 100)
+        rd.close()
+
+        rd = store.get_reader(b'test-storage-overwrite', Kind.CONFIG)
+        wvpasseq(rd.read(), b'b' * 100)
+        wvstart("seek")
+        wvpasseq(rd.read(), b'')
+        rd.seek(0)
+        wvpasseq(rd.read(), b'b' * 100)
+        rd.seek(90)
+        wvpasseq(rd.read(), b'b' * 10)
+        rd.seek(90)
+        wvpasseq(rd.read(10), b'b' * 10)
+        rd.close()
+
+        wvstart("not found")
+        wvexcept(FileNotFound, store.get_reader, b'test-404', Kind.CONFIG)
+
+@wvtest
+def test_storage_packs():
+    with no_lingering_errors(), create_test_config() as (tmpdir, store):
+        kinds = {
+            Kind.METADATA: ("METADATA", b"mpack"),
+            Kind.DATA: ("DATA", b"dpack"),
+            Kind.IDX: ("IDX", b"idx"),
+        }
+        for kind, (kindname, ext) in kinds.items():
+            wvstart("create a new file %s" % kindname)
+            filename = b'pack-zzzzzzz.%s' % ext
+            wr = store.get_writer(filename, kind)
+            wr.write(b'a' * 100)
+            wr.close()
+
+            for nkind, (nkindname, _) in kinds.items():
+                wvstart("cannot overwrite by %s" % nkindname)
+                wvexcept(FileAlreadyExists, store.get_writer,
+                         filename, kind)
+                rd = store.get_reader(filename, kind)
+                wvexcept(Exception, store.get_writer,
+                         filename, kind, overwrite=rd)
+                rd.close()
+
+            wvstart("read back")
+            rd = store.get_reader(filename, kind)
+            wvpasseq(rd.read(), b'a' * 100)
+            rd.close()
